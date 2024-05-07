ecdc9634493b133d355999afe9c3ca53febd7bc1
ACK-J <60232273+ACK-J@users.noreply.github.com>
2024-05-07 17:47:08 +0000

    Bug fixes and added checks for dnssec

--- background.js
+++ background.js
@@ -1,6 +1,26 @@
 browser.runtime.onMessage.addListener((message) => {
   if (message.action === "updateBadge") {
-    // Update the badge text with the provided number
-    browser.browserAction.setBadgeText({ text: message.number.toString() });
+    let getStoredTab = parseInt(localStorage.getItem("tabID"), 10);
+    // Update the badge text with the provided number for the current tab
+    browser.browserAction.setBadgeText({ tabId: getStoredTab, text: message.number.toString() });
   }
+});
+
+function onCreated(windowInfo) {
+  browser.windows.update(windowInfo.id, {
+    height: 850,
+    width: 400,
+  });
+}
+
+browser.browserAction.onClicked.addListener(async function(tab) {
+  let pane = browser.windows.create({
+    url: "popup.html",
+    type: "popup",
+  });
+  pane.then(onCreated);
+  // Store the URL of the current tab in storage
+  await localStorage.clear();
+  localStorage.setItem("url", tab.url);
+  localStorage.setItem("tabID", tab.id);
 });
